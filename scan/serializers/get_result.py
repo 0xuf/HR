b18458435638cd9b6dfb50818af697ee9b642383
0xuf <mobin.iwhh@gmail.com>
2022-11-18 17:54:50 +0330

    Update: Clean code

--- scan/serializers/get_result.py
+++ scan/serializers/get_result.py
@@ -1,18 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from scan.models import Target
 from datetime import datetime
 import ast
 
 
-class GetResultSerializer(ModelSerializer):
+class GetResultSerializer(serializers.ModelSerializer):
+    """
+    Get result of scan serializer
+    """
     class Meta:
+        """
+        Meta class
+        """
         model = Target
         fields = ["scan_id"]
 
-    def to_representation(self, instance):
+    def to_representation(self, instance) -> dict:
+        """
+        Override to_representation to customize response
+        :param instance: received instance from request
+        """
+
+        # define response dict and subdomains list
         response_dict = dict()
         subdomains = list()
 
+        # Loop into subdomains of instance to make list from them
         for subdomain in instance.subdomains.all():
             subdomains.append(
                 {
@@ -22,4 +35,12 @@ class GetResultSerializer(ModelSerializer):
             )
         response_dict["date"] = datetime.now().strftime("%Y-%m-%d")
         response_dict["subdomains"] = subdomains
+
         return response_dict
+
+
+class PagenumSerializer(serializers.Serializer):  # noqa
+    """
+    Page number serializer
+    """
+    page_num = serializers.IntegerField()
