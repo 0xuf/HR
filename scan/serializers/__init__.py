e022f91b5b633f6560d246aa073fb16910e0d71b
0xuf <mobin.iwhh@gmail.com>
2022-11-18 17:55:09 +0330

    Create: __init__.py

--- scan/serializers/__init__.py
+++ scan/serializers/__init__.py
@@ -2,4 +2,6 @@ from scan.serializers.create_new_target import (
     CreateNewTargetSerializer, ViewTargetSerializer
 )
 
-from scan.serializers.get_result import GetResultSerializer
+from scan.serializers.get_result import (
+    GetResultSerializer, PagenumSerializer
+)
