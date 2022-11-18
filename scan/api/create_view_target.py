4c55db8c63029e344abb22721f16baecf5211485
0xuf <mobin.iwhh@gmail.com>
2022-11-18 07:10:11 +0330

    Create: Create/View target class

---
+++ scan/api/create_view_target.py
@@ -0,0 +1,43 @@
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from scan.serializers import (CreateNewTargetSerializer, ViewTargetSerializer)
+from scan.models import Target
+from uuid import uuid4
+from scan.tasks import scan_target
+
+
+class CreateViewTarget(ListCreateAPIView):
+    """
+    Create new target api class
+    """
+    serializer_class = ViewTargetSerializer
+    create_target_serializer = CreateNewTargetSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Target.objects.filter(user=self.request.user)  # noqa
+
+    def create(self, request, *args, **kwargs):
+        """
+        Create method to create target in database using CreateNewTargetSerializer
+        """
+        _uuid = uuid4()
+
+        serialized_data = self.create_target_serializer(data=dict(
+            domain=self.request.POST.get("domain")
+        ),
+            context=dict(
+                user=self.request.user,
+                uuid=_uuid
+            )
+        )
+
+        # Check if the values are valid, return a response with a status code of 201, and if it is wrong, 400
+        if serialized_data.is_valid():
+            serialized_data.save()
+            scan_target.delay(serialized_data.data.get("domain"))
+            return Response({"scan_id": _uuid}, status=status.HTTP_201_CREATED)
+
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
