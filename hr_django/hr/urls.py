4d5bdd7e0b06b3a7b77f50957ea3fc40a3bbf852
0xuf <mobin.iwhh@gmail.com>
2022-11-18 23:38:32 +0330

    Refactor

--- hr_django/hr/urls.py
+++ hr_django/hr/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
-    url="http://localhost"
+    url=f"http://{settings.ADDRESS}"
 )
 
 urlpatterns = [
