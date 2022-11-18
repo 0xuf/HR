d77bffc852ad942eb20c6b6fe5427d8df56e51c7
0xuf <mobin.iwhh@gmail.com>
2022-11-18 07:14:42 +0330

    Update: added api/ route

--- hr/urls.py
+++ hr/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/', include('users.urls')),
+    path("admin/", admin.site.urls),
+    path("api/user/", include("users.urls")),
+    path("api/", include("scan.urls")),
 ]
