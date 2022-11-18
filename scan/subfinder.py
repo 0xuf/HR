b8bac012eb79166db65ecacce9f864a26bd64a66
0xuf <mobin.iwhh@gmail.com>
2022-11-18 18:03:34 +0330

    Update: clean code

--- scan/subfinder.py
+++ scan/subfinder.py
@@ -3,12 +3,22 @@ from utilities.subdomain_check import is_valid_subdomain
 
 
 class Subfinder:
+    """
+    Subfinder class
+    """
     subdomains: list = list()
 
     def __init__(self, domain: str = None) -> None:
+        """
+        Initial method of subfinder class
+        :param domain: received domain from request
+        """
         self.domain = domain
 
     def get_subdomains(self) -> list:
+        """
+        get_subdomains will list subdomains within subfinder script
+        """
 
         # Get all subdomains using subfinder script
         subfinder_command = subprocess.Popen(
@@ -17,11 +27,14 @@ class Subfinder:
             stderr=subprocess.PIPE
         ).communicate()
 
+        # define output and error from subfinder output
         out, err = subfinder_command[0].decode("UTF-8").splitlines(), subfinder_command[1].decode("UTF-8")
 
+        # Return an empty list if subfinder has errors
         if len(err) != 0:
             return list()
 
+        # Loop into subfinder output to validate subdomains and make list from them
         for subdomain in out:
             self.subdomains.append(subdomain) if is_valid_subdomain(self.subdomains, subdomain, self.domain) else ...
 
--- scan/tasks.py
+++ scan/tasks.py
@@ -9,35 +9,49 @@ from scan.models import (
 
 @shared_task()
 def scan_target(domain: str) -> None:
+    """
+    Scan target function created for scan received target from user in background within celery
+    :param domain: received domain from request
+    """
 
+    # Subfinder instance
     subfinder_instance = Subfinder(domain)
     subfinder_output = subfinder_instance.get_subdomains()
+
+    # target object
     target = Target.objects.get(domain=domain)  # noqa
 
+    # Nuclei on subdomains of subfinder works
     if subfinder_output:
-
+        # Loop into subdomains of subfinder output
         for subdomain in subfinder_output:
-
+            # Check target is up or not
             try:
                 ip_address = socket.gethostbyname(subdomain)
             except socket.gaierror:
                 continue
 
+            # Subdomain instance
             sub_object = Subdomain(
                 target=target,
                 subdomain=subdomain,
                 ip_address=ip_address
             )
+
+            # Nuclei instance
             nuclei_instance = Nuclei(subdomain)
             nuclei_out = nuclei_instance.nuclei()
 
+            # Save nuclei result into subdomain instance if has result
             if nuclei_out:
                 sub_object.nuclei_result = nuclei_out
                 sub_object.save()
 
+        # Change target status to finished
         target.status = "finished"
         target.save()
 
     else:
+        # Change target status to error
         target.status = "error"
         target.save()
