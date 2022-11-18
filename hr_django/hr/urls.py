6d3f0bfb14f78bf6858066c041afa2056d7bee49
0xuf <mobin.iwhh@gmail.com>
2022-11-18 19:18:59 +0330

    Update: added drf yasg settings and urls

--- hr_django/hr/urls.py
+++ hr_django/hr/urls.py
@@ -15,10 +15,33 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="HamRavesh API",
+        default_version='v1',
+        description="Hamravesh tasks",
+        terms_of_service="https://hamravesh.com/",
+        contact=openapi.Contact(email="feyzian.mobin@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    url=settings.domain
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("users.urls")),
     path("api/", include("scan.urls")),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  # noqa
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  # noqa
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
