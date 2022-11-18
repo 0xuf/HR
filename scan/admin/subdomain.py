002a3463c9c51e771ee8e3da6006263a2e4c03ec
0xuf <mobin.iwhh@gmail.com>
2022-11-18 18:08:08 +0330

    Update: clean code

--- scan/admin/subdomain.py
+++ scan/admin/subdomain.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 
 class SubdomainAdmin(admin.ModelAdmin):
+    """
+    Subdomain admin object in django admin panel
+    """
     pass
--- scan/admin/target.py
+++ scan/admin/target.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 
 class TargetAdmin(admin.ModelAdmin):
+    """
+    Target admin object in django panel
+    """
     pass
